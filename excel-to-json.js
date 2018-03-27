8d2007063f1b8ebdc400f6ba832dd38dacf54e78
uwangg <uwangg@gmail.com>
2018-03-27 17:30:30 +0900

    sheet_to_json -> 직접 파싱으로 변경
    일부 excel파일이 sheet_to)json이 안되는 경우가 있어 worksheet안에 데이터 직접 파싱하는 방법으로 변경

--- excel-to-json.js
+++ excel-to-json.js
@@ -5,11 +5,47 @@ function readExcelFile(fileName, sheetIdx) {
 	var workbook = XLSX.readFile(fileName);
 	var sheetNameList = workbook.SheetNames;	// sheetList
 	var worksheet = workbook.Sheets[sheetNameList[sheetIdx]];	// 선택한 sheet 내용
-	return XLSX.utils.sheet_to_json(worksheet);
+	
+	var headers = {};
+	var data = [];
+	for( z in worksheet ) {
+		if( z[0] === '!' ) continue;
+		var col = z.substring(0, 1);	// A, B, C, D ..
+		var row = parseInt(z.substring(1));	// 1, 2, 3, 4 ..
+		var value = worksheet[z].v;
+
+		// header 생성
+		if( row == 1 ) {
+			headers[col] = value;
+			continue;
+		}
+
+		if( !data[row] ) data[row] = {};
+		data[row][headers[col]] = value;
+	}
+	// 2번째부터 시작했으므로 shift처리
+	data.shift();
+	data.shift();
+	return data;
 };
 
-var fileName = "동네예보조회서비스_격자_위경도.xlsx";
-var sheetIdx = 0;
-var data = readExcelFile(fileName, sheetIdx);
+// var fileName = "동네예보조회서비스_격자_위경도.xlsx";
+// var sheetIdx = 0;
+// var data = readExcelFile(fileName, sheetIdx);
 
-console.log(data);
+var fileList = [{
+	"fileName": "./data/동네예보조회서비스_격자_위경도.xlsx",
+	"sheetIdx": 0
+},
+{
+	"fileName": "./data/KIKmix.20180301.xlsx",
+	"sheetIdx": 0
+}];
+
+var dataList = [];
+for(var i = 0 ; i < fileList.length ; i++){
+	if(i == 0) continue;
+	var file = fileList[i];
+	dataList.push(readExcelFile(file['fileName'], file['sheetIdx']));
+}
+console.log(dataList[0][0]);
